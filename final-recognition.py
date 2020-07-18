8573417d25138cc0765c59fd0199914c0a74a190
Jacek Graczyk <jaca173@gmail.com>
2020-07-18 22:01:36 +0200

    removeKartkens code merged

--- final-recognition.py
+++ final-recognition.py
@@ -1,33 +1,30 @@
 # from 01_perspective import *
-from perspective import perspective_operations, get_image, save_image
 from checkered import checkered_operations
+from perspective import perspective_operations
+from utils import get_image, save_image
+from removeKartkens import removeLines
 from tqdm import tqdm
+
 
 def shoggoth(img_num):
     image = get_image(img_num)
     # Wykrycie narożników kartki, skorygowanie perspektywy oraz wycięcie kartki.
     image_perspective = perspective_operations(image)
     # Usunięcie kratek z kartki Tak, żeby zostały widoczne tylko słowa 
-    image_checkered = checkered_operations(image_perspective)
+    image_checkered = removeLines(image_perspective)
 
-    # Zapisanie obrazka  
-    # image_to_compare_01 = resize_image_height(image, 600)
-    # image_to_compare_02 = resize_image_height(image_checkered, 600)
-
-    # numpy_horizontal_concat = np.concatenate((image_to_compare_01, image_to_compare_02), axis=1)
     save_image(img_num, image_checkered)
 
-    # Znajdowanie wierszy 
     # Znajdowanie wyrazów 
     # Wykrycie pojedynczych cyfr
     # Rozpoznanie cyfr
 
+
 # Etap 1:
 # Nie działa dla:
-# image_range = [7, 8, 11, 16, 17, 21, 22, 23, 26] 
-image_range = range(1,30)
+# image_range = [7, 8, 11, 16, 17, 21, 22, 23, 26]
+image_range = range(1, 30)
 
 for image_num in tqdm(image_range):
     print('image num: {}'.format(image_num))
     shoggoth(image_num)
-    
---
+++ removeKartkens.py
@@ -0,0 +1,137 @@
+import cv2
+import math
+import numpy as np
+import os
+from tqdm import tqdm
+# from utils import get_image
+from scipy.signal import convolve2d
+import cv2
+
+# data_dir = 'out'
+# out_dir = 'out2'
+
+
+def eight_neighbor_average_convolve2d_horizontal(x):
+    kernel = np.array([
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+                       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    ])
+
+    neighbor_sum = convolve2d(
+        x, kernel, mode='same',
+        boundary='fill', fillvalue=0)
+
+    num_neighbor = convolve2d(
+        np.ones(x.shape), kernel, mode='same',
+        boundary='fill', fillvalue=0)
+
+    return neighbor_sum / num_neighbor
+
+
+def eight_neighbor_average_convolve2d_vertical(x):
+    kernel = np.array([
+                       [1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                       [1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+    ])
+
+    neighbor_sum = convolve2d(
+        x, kernel, mode='same',
+        boundary='fill', fillvalue=0)
+
+    num_neighbor = convolve2d(
+        np.ones(x.shape), kernel, mode='same',
+        boundary='fill', fillvalue=0)
+
+    return neighbor_sum / num_neighbor
+
+
+def removeLines(image):
+    image = np.array(image)
+    # croppedImage = image[:, int(len(image[0])/2):]
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    
+
+    image_swaped_colors = cv2.bitwise_not(image)
+
+    bw = cv2.adaptiveThreshold(image_swaped_colors, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                               cv2.THRESH_BINARY, 15, -2)
+    
+    horizontal = np.copy(bw)
+    vertical = np.copy(bw)
+
+    cols = horizontal.shape[1]
+    horizontal_size = cols // 80
+    # Create structure element for extracting horizontal lines through morphology operations
+    horizontalStructure = cv2.getStructuringElement(
+        cv2.MORPH_RECT, (horizontal_size, 1))
+    # Apply morphology operations
+    horizontal = cv2.erode(horizontal, horizontalStructure)
+    horizontal = cv2.dilate(horizontal, horizontalStructure)
+
+    rows = vertical.shape[0]
+    verticalsize = rows // 80
+    # Create structure element for extracting vertical lines through morphology operations
+    verticalStructure = cv2.getStructuringElement(
+        cv2.MORPH_RECT, (1, verticalsize))
+    # Apply morphology operations
+    vertical = cv2.erode(vertical, verticalStructure)
+    vertical = cv2.dilate(vertical, verticalStructure)
+
+
+    verticalTrue = [vertical > 128]
+    horizontalTrue = [horizontal > 128]
+
+    x_ver = eight_neighbor_average_convolve2d_horizontal(image)
+    x_hor = eight_neighbor_average_convolve2d_vertical(image)
+
+    print("img 0 0", image[0,0])
+
+    print(x_ver.shape)
+
+    print("x_hor", x_hor[0,0])
+    print("verticalTrue", verticalTrue[0][0,0])
+
+    lineindex = 0
+    assert len(image) == len(verticalTrue[0])
+    for line, trueLine in zip(image, verticalTrue[0]):
+        index = 0
+        for pix, shouldChange in zip(line, trueLine):
+            if shouldChange:
+                image[lineindex, index] = int(x_ver[lineindex, index])
+            index += 1
+        lineindex += 1
+
+    lineindex = 0
+    assert len(image) == len(horizontalTrue[0])
+    for line, trueLine in zip(image, horizontalTrue[0]):
+        index = 0
+        for pix, shouldChange in zip(line, trueLine):
+            if shouldChange:
+                image[lineindex, index] = int(x_hor[lineindex, index])
+            index += 1
+        lineindex += 1
+
+    print("img 0 0", image[0,0])
+    return image
