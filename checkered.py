7c6ff50dffdf8331b9df1b337e0f457400378a58
Jacek Graczyk <jaca173@gmail.com>
2020-07-19 09:58:11 +0200

    dombears and viruseks solutions connected

--- checkered.py
+++ checkered.py
@@ -3,9 +3,8 @@ import math
 import numpy as np
 
 def checkered_operations(image):
-  gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-  treshold_image = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 50)
-  kernel_ero = np.ones((3, 3), np.uint8) 
+  treshold_image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 40)
+  kernel_ero = np.ones((3, 3), np.uint8)
   erode_image = cv2.erode(treshold_image, kernel_ero, cv2.BORDER_REFLECT) 
 
   return erode_image
--- final-recognition.py
+++ final-recognition.py
@@ -1,6 +1,7 @@
 # from 01_perspective import *
 from checkered import checkered_operations
 from perspective import perspective_operations
+from words import detect_words_operations
 from utils import get_image, save_image
 from removeKartkens import removeLines
 from tqdm import tqdm
@@ -11,18 +12,17 @@ def shoggoth(img_num):
     # Wykrycie narożników kartki, skorygowanie perspektywy oraz wycięcie kartki.
     image_perspective = perspective_operations(image)
     # Usunięcie kratek z kartki Tak, żeby zostały widoczne tylko słowa 
-    image_checkered = removeLines(image_perspective)
-
-    save_image(img_num, image_checkered)
-
+    image_remove_lines = removeLines(image_perspective)
+    image_checkered = checkered_operations(image_remove_lines)
     # Znajdowanie wyrazów 
+    image_detect_words = detect_words_operations(image_checkered)
+
+    save_image(img_num, image_detect_words)
+
     # Wykrycie pojedynczych cyfr
     # Rozpoznanie cyfr
 
 
-# Etap 1:
-# Nie działa dla:
-# image_range = [7, 8, 11, 16, 17, 21, 22, 23, 26]
 image_range = range(1, 30)
 
 for image_num in tqdm(image_range):
