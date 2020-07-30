7392f0979b2043e3d74095c6e59f3dd36ecc0c5f
Jacek Graczyk <jaca173@gmail.com>
2020-07-30 20:40:52 +0200

    additional function with all operatons created

--- final-recognition.py
+++ final-recognition.py
@@ -12,53 +12,54 @@ from tqdm import tqdm
 from find_lines import crop_small_rectangles, paint_lines, prepare_mask, move_to_original_coords
 from keras.models import load_model
 
-data_dir = 'data'
-out_dir = 'out'
+local_in_dir = 'data'
+local_out_dir = 'out'
+local_image_range = range(1,2)
 pretict_model_path = './final_model.h5'
 
-def shoggoth(img_num):
-    image = get_image(img_num, data_dir)
-    # Wykrycie narożników kartki, skorygowanie perspektywy oraz wycięcie kartki.
-    image_perspective_operations, sPoints, tPoints = perspective_operations(image)
-    # Usunięcie kratek z kartki tak, żeby zostały widoczne tylko słowa 
-    image_remove_lines_operations = removeLines(image_perspective_operations)
-    image_checkered_operations = checkered_operations(image_remove_lines_operations)
-    # Wykrycie słów
-    image_detect_words = detect_words_operations(image_checkered_operations)
-    # Wykrycie indeksów
-    image_crop_numbers, cropped_numbers = crop_numbers(image_detect_words, image_perspective_operations)
-    # Podział na cyfry
-    image_crop_digits = crop_digits(cropped_numbers, image)
+def all_operations(in_dir, image_range, out_dir):
+    def shoggoth(img_num):
+        image = get_image(img_num, in_dir)
+        # Wykrycie narożników kartki, skorygowanie perspektywy oraz wycięcie kartki.
+        image_perspective_operations, sPoints, tPoints = perspective_operations(image)
+        # Usunięcie kratek z kartki tak, żeby zostały widoczne tylko słowa 
+        image_remove_lines_operations = removeLines(image_perspective_operations)
+        image_checkered_operations = checkered_operations(image_remove_lines_operations)
+        # Wykrycie słów
+        image_detect_words = detect_words_operations(image_checkered_operations)
+        # Wykrycie indeksów
+        image_crop_numbers, cropped_numbers = crop_numbers(image_detect_words, image_perspective_operations)
+        # Podział na cyfry
+        image_crop_digits = crop_digits(cropped_numbers, image)
 
-    cropped_rectangles = crop_small_rectangles(image_detect_words)
-    mask = paint_lines(cropped_rectangles)
-    mask = prepare_mask(mask)
-    new_img = move_to_original_coords(mask, sPoints, tPoints, image.shape[:2])
-    save_image(img_num, new_img, "png", out_dir)
+        cropped_rectangles = crop_small_rectangles(image_detect_words)
+        mask = paint_lines(cropped_rectangles)
+        mask = prepare_mask(mask)
+        new_img = move_to_original_coords(mask, sPoints, tPoints, image.shape[:2])
+        save_image(img_num, new_img, "png", out_dir)
 
-    return image_crop_digits
+        return image_crop_digits
 
 
-image_range = range(1,2)
 
-image_crop_digits = []
-for image_num in tqdm(image_range):
-  print('image num: {}'.format(image_num))
-  image_crop_digits.append(shoggoth(image_num))
+    image_crop_digits = []
+    for image_num in tqdm(image_range):
+        image_crop_digits.append(shoggoth(image_num))
 
-index_array = []
+    index_array = []
+    model = tf.keras.models.load_model(pretict_model_path)
 
-model = tf.keras.models.load_model(pretict_model_path)
+    for page_number, page in tqdm(enumerate(image_crop_digits)):
+        index_array.append([])
+        for index_number, image_number in enumerate(page):
+            index_array[page_number].append('')
+            for index_digit, image_digit in enumerate(image_number):
+                predicted_digit = predict_image(image_digit, model)
+                index_array[page_number][index_number] += str(predicted_digit)
+        index_array[page_number].reverse()
 
-for page_number, page in tqdm(enumerate(image_crop_digits)):
-    index_array.append([])
-    for index_number, image_number in enumerate(page):
-        index_array[page_number].append('')
-        for index_digit, image_digit in enumerate(image_number):
-            predicted_digit = predict_image(image_digit, model)
-            index_array[page_number][index_number] += str(predicted_digit)
-    index_array[page_number].reverse()
+    for image_num, page in enumerate(index_array):
+        save_txt((image_num + 1), page, out_dir)
+        print('page: {}'.format(page))
 
-for image_num, page in enumerate(index_array):
-    save_txt((image_num + 1), page, out_dir)
-    print('page: {}'.format(page))
+# all_operations(local_in_dir, local_image_range, local_out_dir)
