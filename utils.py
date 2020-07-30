d492cce06ce1c6fa6cd294353eff48a7baf55740
Dombearx <domin.dober@gmail.com>
2020-07-30 14:29:59 +0200

    Add function to save txt

--- utils.py
+++ utils.py
@@ -10,10 +10,25 @@ def get_image(img_num, data_dir):
     return image_to_show
 
 
+def save_txt(img_num, string_list, out_dir):
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    file_path = './{}/{}-indeksy.txt'.format(out_dir, img_num)
+    f = open(file_path, "w")
+    for digit_list in string_list:
+        digits = "".join(digit_list)
+
+        f.write(digits + "\n")
+
+    f.close()
+    print('Saved to {}'.format(file_path, ))
+
+
 def save_image(img_num, image, extension, out_dir):
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
-    image_path = './{}/img_{}.{}'.format(out_dir, img_num, extension)
+    image_path = './{}/{}-wyrazy.{}'.format(out_dir, img_num, extension)
     cv2.imwrite(image_path, image)
     print('Saved to {}'.format(image_path, ))
